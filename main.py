d2012835c61ed302ede30a196b569c53c26455fd
mrwitzbold <mrwitzbold@gmail.com>
2024-03-11 00:40:52 -0300

    duplicate detection and searching

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def help():
-    print("\n\n================================================\n")
-    print("help - get help\n")
-    print("register_person \{person_name\}\n")
+    print("\ n\n================================================\n")
+    print("help - get help")
+    print("register_person {person_name} - register new person")
+    print("search_person {name or part of name} - search in people database")
     print("\n================================================\n\n")
 
 def register_person(name):
@@ -11,33 +12,72 @@ def register_person(name):
         person_list_file = open("data/person_list.hole", "r")
         person_list = person_list_file.readlines()
         for line in person_list:
-            if line == "NAME=" + name:
+            if "NAME=" + name in line:
                 sure = input("Duplicate detected, are you sure? y/n")
                 if(sure != "y"):
                     register = False
     except Exception as e:
         person_list_file = open("data/person_list.hole", "w").close()
+    if register == True:
+        person_list_file = open("data/person_list.hole", "a")
+        person_list_file.write("\n\nSTART\n")
+        person_list_file.write("NAME=" + name)
+        person_list_file.write("\nID=")
+        person_list_file.write("\nBIRTH=")
+        person_list_file.write("\nNATIONALITY=")
+        person_list_file.write("\nRACE=")
+        person_list_file.write("\nPHONE_NUMBERS=")
+        person_list_file.write("\nWORST_FEAR=")
+        person_list_file.write("\nEND")
+        person_list_file.close()
+        print("\nSuccessfully registered new person " + name)
+    else:
+        print("Didn't register any new people.")
 
-    person_list_file = open("data/person_list.hole", "a")
-    person_list_file.write("\n\nSTART\n")
-    person_list_file.write("NAME=" + name)
-    person_list_file.write("\nID=")
-    person_list_file.write("\nBIRTH=")
-    person_list_file.write("\nNATIONALITY=")
-    person_list_file.write("\nRACE=")
-    person_list_file.write("\nPHONE_NUMBERS=")
-    person_list_file.write("\nWORST_FEAR=")
-    person_list_file.write("END")
-    person_list_file.close()
-    print("\nSuccessfully registered new person " + name)
+def search_people(name):
+    print_line = False
+    person_list = open("data/person_list.hole", "r").readlines()
+    for line in person_list:
+        if ("NAME=" in line) and (name in line):
+            print_line = True
+        if "END" in line:
+            print_line = False
+            print("\n\n")
+
+        if print_line == True:
+            print(line)
+            
+
+#def delete_person(name)
+ #   person_list_file = open("data/person_list.hole", "r")
+  #  person_list = person_list_file.readlines()
+    #for line in person_list:
+    #    if()
 
 
 help()
 while True:
     command = input("\n> ")
+    arg1 = ""
+    arg2 = ""
+    try:
+        arg1 = command.split(" ")[0]
+        arg2 = command.split(" ")[1]
+    except Exception as e:
+        print("")
     
     if(command == "help"):
         help()
 
-    if(command.split(" ")[0] == "register_person"):
-        register_person(command.split(" ")[1])
+    if(arg1 == "register_person"):
+        register_person(arg2)
+
+    if(arg1 == "search_person"):
+        search_people(arg2)
+
+
+
+
+
+
+
